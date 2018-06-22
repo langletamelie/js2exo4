5b06b55ff83712a1b41af0554ad2b1e7b5394dea
langletamelie <amelie.langlet@hotmail.com>
2018-06-22 15:01:50 +0200

    exo4

---
+++ assets/js/js4.js
@@ -0,0 +1,8 @@
+document.getElementById('RAZ').onclick = function (){change()};
+//déclaration de la fonction
+function change(){
+  //changement des valeurs des champs
+document.getElementById('lastname').value ="";
+document.getElementById('firstname').value ="";
+document.getElementById('city').value ="";
+}
